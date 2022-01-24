1db0cd7f9f0fe615f1840ff5703023e76545e8ad
Xianke4 <98265957+Xianke4@users.noreply.github.com>
2022-01-24 04:07:32 -0600

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
+    if (score <= 49) return '菜';
+    if (score <= 99) return '梁浩：就这？';
+    if (score <= 149) return '梁浩：啊啊啊啊';
+    if (score <= 199) return '艹，牛啊';
     return '人？';
 }
 
